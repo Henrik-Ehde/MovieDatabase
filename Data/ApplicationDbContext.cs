5504866f65de2ed93f27a08e89c988f4edd5f52b
Henrik-Ehde <henrik_ehde@hotmail.com>
2024-05-06 15:58:00 +0200

    added data seeding and finalized the project

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,10 +1,13 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
 using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.BlazorIdentity.Pages.Manage;
+using MovieDatabase.Data.Migrations;
 using MovieDatabase.Models;
 using NuGet.ContentModel;
 using System.Reflection.Emit;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MovieDatabase.Data
 {
@@ -22,6 +25,92 @@ namespace MovieDatabase.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            //SEED GENRES
+            builder.Entity<Genre>().HasData(new Genre {Id=1, Name="Comedy" });
+            builder.Entity<Genre>().HasData(new Genre {Id=2, Name= "Drama" });
+            builder.Entity<Genre>().HasData(new Genre {Id=3, Name= "Action" });
+            builder.Entity<Genre>().HasData(new Genre {Id=4, Name= "Romance" });
+            builder.Entity<Genre>().HasData(new Genre {Id=5, Name= "Science Fiction" });
+            builder.Entity<Genre>().HasData(new Genre {Id=6, Name= "Fantasy" });
+            builder.Entity<Genre>().HasData(new Genre {Id=7, Name= "Children" });
+
+
+            //SEED MOVIES
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 1,
+                    Title = "Star Wars",
+                    Description = "Luke Skywalker learns magic from a strange hermit in the desert, joins the rebels and fights the empire",
+                    ReleaseDate = DateOnly.Parse("16 december 1977"),
+                    GenreId = 5,
+                    ImgFileName= "Star Wars.jpg"
+                });
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 2,
+                    Title = "Dune Part Two",
+                    Description = "Paul Atreides starts a holy war",
+                    ReleaseDate = DateOnly.Parse("2024-02-28"),
+                    GenreId = 5,
+                    ImgFileName = "Dune Part Two.jpg"
+                });
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 3,
+                    Title = "This is the end",
+                    Description = "A bunch of famous actors face the apocalypse and die",
+                    ReleaseDate = DateOnly.Parse("2013-06-03"),
+                    GenreId = 1,
+                    ImgFileName = "This-is-the-End-Film-Poster.jpg"
+                });
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 4,
+                    Title = "Ice Age",
+                    Description = "A mammoth, a sloth and a saber-toothed tiger find a human baby and go on a journey to find its family.",
+                    ReleaseDate = DateOnly.Parse("2002-03-15"),
+                    GenreId = 7,
+                    ImgFileName = "Ice Age.webp"
+                });
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 5,
+                    Title = "Lunana: A Yak in the Classroom",
+                    Description = "A teacher who doesn't want to teach is transferred to the most remote school in the world.",
+                    ReleaseDate = DateOnly.Parse("2019-10-05"),
+                    GenreId = 2,
+                    ImgFileName = "A Yak in the Classroom.jpg"
+                });
+
+            builder.Entity<Movie>().HasData(
+                new Movie
+                {
+                    Id = 6,
+                    Title = "Parasite",
+                    Description = "Greed and class discrimination threaten the newly-formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+                    ReleaseDate = DateOnly.Parse("2019-05-30"),
+                    GenreId = 1,
+                    ImgFileName = "Parasite.jpg"
+                });
+
+
+
+
+
+
+
+            //Create an admin role and an admin user
 
             string ADMIN_ID = "02174cf0–9412–4cfe - afbf - 59f706d72cf6";
             string ROLE_ID = "341743f0-asd2–42de-afbf-59kmkkmk72cf6";
--- Data/Migrations/20240506133900_add-migration SeedData.Designer.cs
+++ Data/Migrations/20240506133900_add-migration SeedData.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MovieDatabase.Data;
 
@@ -11,9 +12,11 @@ using MovieDatabase.Data;
 namespace MovieDatabase.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240506133900_add-migration SeedData")]
+    partial class addmigrationSeedData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
